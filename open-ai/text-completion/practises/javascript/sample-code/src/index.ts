dbca632c32dab73f272e74c54d16de1dcab3ef02
Leo <leo.nguyen.goldenowl@gmail.com>
2023-02-05 23:53:15 +0700

    write openai text completion and sample code

---
+++ open-ai/text-completion/practises/javascript/sample-code/src/index.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+import type { CreateCompletionRequest } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completionRequest: CreateCompletionRequest = {
+  model: "text-davinci-003",
+  prompt: "How to become a good person?",
+  temperature: 0,
+  max_tokens: 3999,
+  top_p: 1,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+};
+
+openai
+  .createCompletion(completionRequest)
+  .then((res) => console.log("Response:", res.data.choices[0].text))
+  .catch((error) => console.error("Error:", error));
